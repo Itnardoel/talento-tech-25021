bb59a1e1962627abfb80d56fa539c653cd0e4e34
Nicolas Pedranti <nicopedranti@gmail.com>
2025-05-23 16:32:20 -0300

    feat: add providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router";
+import { CartProvider } from "./context/CartProvider.tsx";
 import App from "./App.tsx";
+import "./index.css";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CartProvider>
   </StrictMode>,
 );
