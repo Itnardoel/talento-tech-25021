c6673c00abe664e1782d4a20da37f7fd76f35726
Nicolas Pedranti <nicopedranti@gmail.com>
2025-05-23 16:33:13 -0300

    feat: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-gray-300 p-2.5 text-center text-black">
+      <p> © {new Date().getFullYear()} - Mi Aplicación React</p>
+    </footer>
+  );
+};
