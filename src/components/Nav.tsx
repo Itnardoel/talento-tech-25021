9c55fdb4440f57f3069c87c237b7b7d10ba723b2
Nicolas Pedranti <nicopedranti@gmail.com>
2025-05-23 19:47:49 -0300

    chore: remove unused links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
+import { useCart } from "@/hooks/use-cart";
 import { Link } from "react-router";
 
 export const Nav = () => {
+  const { cart } = useCart();
+
   return (
     <nav className="bg-gray-600 p-2.5 text-white">
       <ul className="m-0 flex list-none justify-around">
@@ -8,13 +11,7 @@ export const Nav = () => {
           <Link to="/">Inicio</Link>
         </li>
         <li>
-          <Link to="#">Acerca de</Link>
-        </li>
-        <li>
-          <Link to="#">Contacto</Link>
-        </li>
-        <li>
-          <Link to="/cart">Carrito</Link>
+          <Link to="/cart">Carrito {cart.length === 0 ? "" : cart.length}</Link>
         </li>
       </ul>
     </nav>
