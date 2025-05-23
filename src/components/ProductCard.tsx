95c618f86a4127216aa5c381b713281ce177de50
Nicolas Pedranti <nicopedranti@gmail.com>
2025-05-23 19:59:25 -0300

    chore: update styles

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -24,7 +24,7 @@ export const ProductCard = ({ product }: ProductCardProps) => {
           onClick={() => {
             navigate(`/product/${product.id}`);
           }}
-          className="cursor-pointer self-center rounded-full bg-gray-400 px-4 py-2"
+          className="cursor-pointer self-center rounded-lg bg-gray-400 px-4 py-2"
         >
           Detalles
         </button>
--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -67,7 +67,7 @@ export const ProductDetail = ({ products }: ProductDetailProps) => {
             onClick={() => {
               handleAddProduct({ productToAdd: productById, count });
             }}
-            className="cursor-pointer rounded-lg bg-gray-500 font-bold"
+            className="cursor-pointer rounded-lg bg-gray-500 px-4 py-2 font-bold"
           >
             Agregar al carrito
           </button>
