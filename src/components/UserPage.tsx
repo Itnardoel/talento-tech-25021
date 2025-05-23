37140d46bd03a87ac7593e0ceea39a204f3b70b8
Nicolas Pedranti <nicopedranti@gmail.com>
2025-05-23 20:28:50 -0300

    feat: add UserPage component

---
+++ src/components/UserPage.tsx
@@ -0,0 +1,18 @@
+import { useUser } from "@/hooks/use-user";
+
+export const UserPage = () => {
+  const { user, handleLogin, handleLogout } = useUser();
+
+  return (
+    <main className="grid place-content-center gap-2">
+      <h2 className="text-2xl font-bold">Logueate para ver la ruta privada</h2>
+      <button
+        type="button"
+        onClick={user ? handleLogout : handleLogin}
+        className="cursor-pointer rounded-lg bg-gray-500 px-4 py-2"
+      >
+        {user ? "Logout" : "Login"}
+      </button>
+    </main>
+  );
+};
