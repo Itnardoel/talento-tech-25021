4a9cb2a9c88bc97bdf1a00e9066d12636c0b915b
Nicolas Pedranti <nicopedranti@gmail.com>
2025-07-07 21:32:26 -0300

    style: Improve layout and styling in AdminProductForm, LoginForm, and UserPage components

--- src/features/user/components/LoginForm.tsx
+++ src/features/user/components/LoginForm.tsx
@@ -20,11 +20,12 @@ export const LoginForm = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    navigate("/");
     if (formValues.email === "admin" && formValues.password === "1234") {
+      navigate("/");
       handleLogin({ ...formValues, role: "ADMIN" });
       toast.success("Ingresaste con éxito como Admin");
     } else if (formValues.email === "user" && formValues.password === "1234") {
+      navigate("/");
       handleLogin(formValues);
       toast.success("Ingresaste con éxito como User");
     } else {
@@ -33,17 +34,23 @@ export const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+    <form
+      onSubmit={handleSubmit}
+      className="flex w-full max-w-80 flex-col gap-2"
+    >
       <h2 className="text-2xl font-bold">Iniciar sesión</h2>
 
       <label className="flex flex-col gap-2">
         Email:
         <input
           type="text"
+          name="email"
+          autoComplete="off"
           value={formValues.email}
           onChange={(event) => {
             setFormValues({ ...formValues, email: event.target.value });
           }}
+          className="rounded border px-2 py-1"
         />
       </label>
 
@@ -51,16 +58,18 @@ export const LoginForm = () => {
         Contraseña:
         <input
           type="password"
+          name="password"
           value={formValues.password}
           onChange={(event) => {
             setFormValues({ ...formValues, password: event.target.value });
           }}
+          className="rounded border px-2 py-1"
         />
       </label>
 
       <button
         type="submit"
-        className="cursor-pointer rounded-lg bg-gray-500 px-4 py-2"
+        className="mt-2 inline-flex h-10 w-full cursor-pointer items-center justify-center gap-2 rounded-md bg-blue-600 px-4 py-2 font-semibold whitespace-nowrap text-white shadow-lg transition-colors hover:bg-blue-600/90 hover:shadow-xl"
       >
         Iniciar sesión
       </button>
